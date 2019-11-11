555c875f11ae72e1c2d884848f4286a7118af032
EK <qxjj7ay@163.com>
2019-11-11 16:35:42 +0800

    mszj

---
+++ ES6/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/模块化/index.js',
+    output: {
+        path: __dirname,
+        filename: './build/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    }
+}
